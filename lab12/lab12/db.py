9c7e3ce92857a922b7d6191596da5752e698e4c1
Ricardo Alvarado <a20190057@pucp.edu.pe>
2023-11-11 01:47:07 -0500

    a

---
+++ lab12/lab12/db.py
@@ -0,0 +1,21 @@
+LOCAL = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'lab12',
+            'USER': 'postgres',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
+
+GAE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lab12',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '34.42.206.120',
+        'PORT': '5432'
+    }
+}
